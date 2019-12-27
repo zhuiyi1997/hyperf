bcd7bd137974e180ec0b3e259bd0a825222c4fd3
ytc <1099843740@qq.com>
2019-12-27 14:15:01 +0800

    first

---
+++ app/Controller/WebSocketController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * User: ytc
+ * Date: 2019/12/27
+ * Email: <fengfeisanfa@163.com>
+ */
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Hyperf\Contract\OnCloseInterface;
+use Hyperf\Contract\OnMessageInterface;
+use Hyperf\Contract\OnOpenInterface;
+use Swoole\Http\Request;
+use Swoole\Server;
+use Swoole\Websocket\Frame;
+use Swoole\WebSocket\Server as WebSocketServer;
+
+class WebSocketController implements OnMessageInterface, OnOpenInterface, OnCloseInterface
+{
+    public function onMessage(WebSocketServer $server, Frame $frame): void
+    {
+        $server->push($frame->fd, 'Recv: ' . $frame->data);
+    }
+
+    public function onClose(Server $server, int $fd, int $reactorId): void
+    {
+        var_dump('closed');
+    }
+
+    public function onOpen(WebSocketServer $server, Request $request): void
+    {
+        $server->push($request->fd, 'Opened');
+    }
+}
